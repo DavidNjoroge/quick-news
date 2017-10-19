190eb2f64a38eac8d2f439b4432b0f7b118fd7c8
David Mwangi <njorogedavid13@gmail.com>
2017-10-19 19:16:45 +0300

    add new row for techcrunch news

--- app/views.py
+++ app/views.py
@@ -10,11 +10,12 @@ def index():
 
     # getting sources
     sources=get_source()
-    cnn=get_articles('bbc-sport')
+    cnn=(get_articles('cnn'))[0:4]
+    bbc_sport=(get_articles('bbc-sport'))[0:4]
+    techcrunch=(get_articles('TechCrunch'))[0:4]
     # print(sources)
-    trunc=cnn[0:4]
     title='Home - Where you can find all your latest news in one website'
-    return render_template('index.html' ,title=title,sources=sources,cnn=trunc)
+    return render_template('index.html' ,title=title,sources=sources,cnn=cnn ,bbc_sport=bbc_sport,techcrunch=techcrunch)
 
 @app.route('/source/<source_name>')
 def source(source_name):
